ece0ea60c77a3d7c415201d40c9968ecc5f687a1
Romain Brg <spike@bepop.com>
2024-12-18 13:34:06 +0100

    ajout de la classe joueur

---
+++ src/Entites/Joueur.java
@@ -0,0 +1,32 @@
+package Entites;
+
+public class Joueur {
+    private double pv;
+    private double money;
+    
+    
+    public Joueur(){
+        this.pv=0;
+        this.money=0;
+    }
+    
+    public double getPv() {
+    return pv;
+    }
+
+    public void setPv(double pv) {
+        this.pv = pv;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    
+    
+
+}
